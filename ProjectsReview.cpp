09c7fe99d43bdf322cb5af911ba762c283ede50a
Abdalrahman Saed <abdalrahmansaed2004@gmail.com>
2024-12-03 04:17:01 +0200

    Add project files.

---
+++ ProjectsReview.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+string FileName = "data.txt";
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    system("pause>0");
+    return 0;
+}
